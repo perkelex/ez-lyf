8a3a2bc4d20ee042cb3b384a20200316240b3945
Orlando Mitrut <orlando.mitrut@gmail.com>
2024-03-13 21:04:29 +0200

    Added sleep inhibitor

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ from PySide6.QtGui import QAction, QGuiApplication
 from Window import window
 from Tray import tray
 from TrayMenu import tray_menu
+from WindowsInhibitor import WindowsInhibitor
+
+def onExit():
+    QGuiApplication.clipboard().clear()
+    WindowsInhibitor.uninhibit()
+
+WindowsInhibitor.inhibit()
 
 quit_action = QAction("Quit")
 quit_action.triggered.connect(app.quit)
@@ -14,7 +21,9 @@ tray_menu.addAction(quit_action)
 tray.setContextMenu(tray_menu)
 window.show()
 
-app.aboutToQuit.connect(lambda: QGuiApplication.clipboard().clear())
+app.aboutToQuit.connect(onExit)
 
 if __name__ == "__main__":
+
+
     app.exec()
