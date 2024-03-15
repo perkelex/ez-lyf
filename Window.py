cdcb7178153bb79d38c9da8a66a690953d3983ad
Orlando Mitrut <orlando.mitrut@gmail.com>
2024-03-15 02:08:07 +0200

    Added sensitive string obfuscation

--- Window.py
+++ Window.py
@@ -2,6 +2,7 @@ from PySide6.QtWidgets import QWidget, QPushButton, QGridLayout, QLabel
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QGuiApplication
 from functools import partial
+from MildEncryption import hide, reveal, isValidToken
 import json
 
 window = QWidget()
@@ -14,7 +15,8 @@ layout = QGridLayout(window)
 def clipboardAdd(content: str):
     QGuiApplication.clipboard().setText(content)
 
-with open("config.json", "r") as config_file:
+with open("config.json", "r+") as config_file:
+    config_needs_dumping = False
     config = json.load(config_file)
     for row_num, config_button in enumerate(config["buttons"]):
         if config_button["type"] == "email":
@@ -28,7 +30,10 @@ with open("config.json", "r") as config_file:
                 layout.addWidget(button, row_num, col_num)
         elif config_button["type"] == "obscured": # "●"
             button = QPushButton("●" * 10)
-            button.clicked.connect(partial(clipboardAdd, config_button["content"]))
+            if not isValidToken(config_button["content"]):
+                config_button["content"] = hide(config_button["content"])
+                config_needs_dumping = True
+            button.clicked.connect(partial(clipboardAdd, reveal(config_button["content"])))
             layout.addWidget(button, row_num, 0)
             if "hint" in config_button:
                 layout.addWidget(QLabel(config_button["hint"]), row_num, 1)
@@ -36,3 +41,6 @@ with open("config.json", "r") as config_file:
             button = QPushButton(config_button["content"])
             button.clicked.connect(partial(clipboardAdd, config_button["content"]))
             layout.addWidget(button, row_num, 0)
+    if config_needs_dumping:
+        config_file.seek(0)
+        json.dump(config, config_file, indent=4)
